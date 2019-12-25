e01f2e452dcd8a72a6a1d565b023324270ce89b5
Neel Kuila <neelk1123@gmail.com>
2019-12-25 16:00:50 -0500

    create endpoint with random string

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module requestbin
 
 go 1.13
 
-require github.com/google/go-cmp v0.3.1
+require (
+	github.com/google/go-cmp v0.3.1
+	github.com/uklineale/go-randutils v0.0.0-20191225203049-a8bb429f7d83
+)
--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"github.com/uklineale/go-randutils"
 )
 
+const BinEndpoint = "/bin/"
+const CreateEndpoint = "/create"
+
+
+func loadHandler(writer http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len(BinEndpoint):]
+	bin, err := loadBin(id)
+	if err != nil {
+		fmt.Fprintf(writer, "<h1>%s</h1>", "404 Not found")
+	}
+
+	fmt.Fprintf(writer, "<h1>%s</h1><div>%s</div>", bin.Id, bin.Requests)
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	id := randutils.String(16)
+	fmt.Println("Making bin with id: ", id)
+
+	bin := &Bin{Id: id, Requests: make([]byte, 100)}
+	bin.save()
+
+	http.Redirect(w, r, BinEndpoint + id, http.StatusFound)
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Starting server")
+	http.HandleFunc(BinEndpoint, loadHandler)
+	http.HandleFunc(CreateEndpoint, createHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
